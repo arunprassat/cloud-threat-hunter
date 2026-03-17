efa6f9454970d499583c007d9ffe5af00306bd6b
Sathy <your-email@gmail.com>
2026-03-17 14:30:49 +0530

    final stable version

--- log_generator.py
+++ log_generator.py
@@ -2,20 +2,19 @@ import time
 import random
 import csv
 from datetime import datetime
+import os
+
+file_name = "logs.csv"
 
 users = ["john", "alice", "bob", "admin"]
 ips = ["192.168.1.1", "10.0.0.1", "172.16.0.5"]
 status = ["success", "failed"]
 
-file_name = "logs.csv"
-
 # Create file with header if not exists
-try:
-    with open(file_name, "x", newline="") as f:
+if not os.path.exists(file_name):
+    with open(file_name, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["user", "ip", "time", "status"])
-except FileExistsError:
-    pass
 
 print("🚀 Generating live logs...")
 
@@ -33,4 +32,4 @@ while True:
 
     print("New log:", log)
 
-    time.sleep(2)  # new log every 2 seconds
+    time.sleep(2)
